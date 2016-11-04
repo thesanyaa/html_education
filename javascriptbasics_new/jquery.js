1c387c46ceaf58a5660488c46bd15f82705bfbdd
Csetneki Sándor <sandor.csetneki@ge.com>
2016-11-04 11:44:24 +0100

    jquery dropdown

--- javascriptbasics_new/jquery.js
+++ javascriptbasics_new/jquery.js
@@ -1,21 +1,43 @@
 window.onload = function()
 {
-  var obj,obj2,obj3;
-  var promise = $.get("user.json", function (data) {
+  var obj, obj2,obj3
+//   var promise = $.get("user.json", function (data) {
+//
+// })
+//
+//
+//   var promise2 = $.get("user2.json", function (data) {
+//   })
+//
+//   var promise3 = $.get("user3.json", function (data) {
+//   })
+//
+//   Promise.all([promise,promise2,promise3]).then(function (value) {
+//     console.log(value)
+//   })
+
+
+  $.get("user.json", function (data) {
+  }).done(function (data) {
+    obj = data
+    $.get("user2.json", function (data) {
+    }).done(function (data) {
+      obj2 = data
+      $.get("user3.json", function (data) {
+      }).done(function (data) {
+        obj3 = data
+        console.log(obj + obj2 +obj3)
+
+      })
+    })
+
 
   })
 
 
-  var promise2 = $.get("user2.json", function (data) {
-  })
 
-  var promise3 = $.get("user3.json", function (data) {
-  })
 
-  Promise.all([promise,promise2,promise3]).then(function (value) {
-    console.log(obj + obj2 + obj3)
-    console.log(value)
-  })
+
 
 
 }
--- javascriptbasics_new/jquery_dropdowns.js
+++ javascriptbasics_new/jquery_dropdowns.js
@@ -1,3 +1,55 @@
 /**
  * Created by 212585857 on 11/4/2016.
  */
+
+$(document).ready(function () {
+  var menu1
+  $.get("dropdown1.json", function (data) {
+     menu1 = data
+  }).done(function (data) {
+    menu1.forEach(function (item) {
+      $('#dropdown1')
+          .append($("<option></option>")
+              .attr("value",item.value)
+              .text(item.text));
+
+    })
+  })
+
+  var menu2
+  $.get("dropdown2.json", function (data) {
+    menu2 = data
+  }).done(function (data) {
+    menu2.forEach(function (item) {
+      $('#dropdown2')
+          .append($("<option></option>")
+              .attr("value",item.value)
+              .text(item.text));
+
+    })
+  })
+
+  $( "#dropdown2" ).on( "choose", function(event, selected) {
+    $("#dropdown2").empty()
+    menu2.forEach(function (item) {
+      if(item.parent === selected) {
+        $('#dropdown2')
+            .append($("<option></option>")
+                .attr("value",item.value)
+                .text(item.text));
+      }
+    })
+  });
+
+  $( "#dropdown1" ).change(function(){
+    console.log("test");
+    var selected = $("#dropdown1 option:selected").val()
+    $( "#dropdown2" ).trigger( "choose", selected )
+  })
+})
+
+
+
+
+
+
